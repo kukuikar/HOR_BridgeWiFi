ac5c0dee00d0b102ab1ca4742b716dc1c47c156a
kukuikar <denis@perepetsky.ru>
2023-12-18 09:34:20 +0300

    main

---
+++ src/main.cpp
@@ -0,0 +1,107 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include <GyverMotor2.h>
+#include <GParser.h>
+// Определяем название и пароль точки доступа
+#include "WIFI_AP.h"
+
+const char TERMINATOR = ';';
+const int BRIDGE_MOTOR_PIN_DIR = 2;
+const int BRIDGE_MOTOR_PIN_PWM = 3;
+
+const int TROLLEY_MOTOR_PIN_DIR = 5;
+const int TROLLEY_MOTOR_PIN_PWM = 6;
+
+const int WINCH_MOTOR_PIN_DIR = 7;
+const int WINCH_MOTOR_PIN_PWM = 1;
+
+const char* HELLO_MSG = "_A_BRIDGE";
+const byte BUFFER_SIZE = 32;
+
+GMotor2<DRIVER2WIRE> MOT_Bridge(  BRIDGE_MOTOR_PIN_DIR,   BRIDGE_MOTOR_PIN_PWM);
+GMotor2<DRIVER2WIRE> MOT_Trolley( TROLLEY_MOTOR_PIN_DIR,  TROLLEY_MOTOR_PIN_PWM);
+GMotor2<DRIVER2WIRE> MOT_Winch(   WINCH_MOTOR_PIN_DIR,    WINCH_MOTOR_PIN_PWM);
+
+WiFiUDP UDP;
+
+void stopAllMotors();
+
+void setup()
+{
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(__SSID, __PSWD); 
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(250);
+  }  
+
+  UDP.beginPacket(WiFi.gatewayIP(), __PORT);
+  UDP.printf(HELLO_MSG);
+  UDP.endPacket();
+
+  UDP.begin(__PORT);
+
+  MOT_Bridge.setMinDuty(70); // мин. ШИМ
+  MOT_Bridge.reverse(1);     // реверс
+  MOT_Bridge.setDeadtime(5); // deadtime
+
+  MOT_Trolley.setMinDuty(70); // мин. ШИМ
+  MOT_Trolley.reverse(1);     // реверс
+  MOT_Trolley.setDeadtime(5); // deadtime
+
+  MOT_Winch.setMinDuty(70); // мин. ШИМ
+  MOT_Winch.reverse(1);     // реверс
+  MOT_Winch.setDeadtime(5); // deadtime
+}
+
+void loop()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    setup();
+  }
+
+  if (UDP.parsePacket())
+  {
+    Serial.print("Received packet! Size: ");
+    Serial.println(UDP.parsePacket());
+    Serial.print("Packet: ");
+    char buffer[BUFFER_SIZE];
+    if (UDP.read(buffer, BUFFER_SIZE))
+    {
+      Serial.println(buffer);
+      GParser data(buffer);
+      int ints[data.amount()];
+      data.parseInts(ints);
+
+      switch (ints[0])
+      {
+      case 0:
+        MOT_Bridge.setSpeed(ints[1]);
+        MOT_Trolley.setSpeed(ints[2]);
+        MOT_Winch.setSpeed(ints[3]);
+        break;
+
+      default:
+        stopAllMotors();
+        break;
+      }
+    }
+  }
+  else
+  {
+    stopAllMotors();
+  }
+}
+
+void stopAllMotors()
+{
+  MOT_Bridge.setSpeedPerc(0);
+  MOT_Trolley.setSpeedPerc(0);
+  MOT_Winch.setSpeedPerc(0);
+}
